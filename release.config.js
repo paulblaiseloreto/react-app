c8e336ca9072fd4232f31d49194d6af6a9061f03
Paul <paul@Ionas-MacBook-Pro.local>
2021-08-16 07:42:54 +0800

    fix

--- release.config.js
+++ release.config.js
@@ -1,17 +1,19 @@
 const { plugins } = require("./build/release.config");
 
 module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/paulblaiseloreto/react-app",
-    plugins: [
-        "@semantic-release/commit-anlayzer",
-        "@semantic-release/release-notes-generator",
-        ["@semantic-release/github", {
-            assets: [
-                { path: "build.zip", label: "Build" },
-                { path: "coverage.zip", label: "Coverage" }
-            ]
-        }
-        ]
-    ]
-}
+  branches: "master",
+  repositoryUrl: "https://github.com/paulblaiseloreto/react-app",
+  plugins: [
+    "@semantic-release/commit-anlayzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
+        ],
+      },
+    ],
+  ],
+};
