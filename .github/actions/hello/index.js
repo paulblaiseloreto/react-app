d5f291e9e3e03e4a02cad93dcede012eb773b081
Paul <paul@Ionas-MacBook-Pro.local>
2021-08-16 17:21:10 +0800

    for merging

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -5,9 +5,9 @@ const github = require("@actions/github");
 try {
   //throw ( new Error ("some error msg"));
 
-  core.debug('Debug message');
-  core.warning('Warning message');
-  core.error('Error message');
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message");
 
   const name = core.getInput("who-to-greet");
   core.setSecret(name);
@@ -16,12 +16,11 @@ try {
   const time = new Date();
   core.setOutput("Time", time.toTimeString());
 
-  core.startGroup('Logging github object')
+  core.startGroup("Logging github object");
   console.log(JSON.stringify(github, null, "\t"));
   core.endGroup();
 
-  core.exportVariable('HELLO', "hello");
-  
+  core.exportVariable("HELLO", "hello");
 } catch (error) {
   core.setFailed(error.message);
 }
