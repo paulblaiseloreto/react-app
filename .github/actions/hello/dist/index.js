2993b37d401fe76942b31e5f1e005754c981e0b2
Paul <paul@Ionas-MacBook-Pro.local>
2021-08-16 09:08:06 +0800

    format checker

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -1,96 +1,102 @@
-module.exports =
-/******/ (function(modules, runtime) { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		var threw = true;
-/******/ 		try {
-/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/ 			threw = false;
-/******/ 		} finally {
-/******/ 			if(threw) delete installedModules[moduleId];
-/******/ 		}
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	__webpack_require__.ab = __dirname + "/";
-/******/
-/******/ 	// the startup function
-/******/ 	function startup() {
-/******/ 		// Load entry module and return exports
-/******/ 		return __webpack_require__(239);
-/******/ 	};
-/******/
-/******/ 	// run startup
-/******/ 	return startup();
-/******/ })
-/************************************************************************/
-/******/ ({
+module.exports = /******/ (function (modules, runtime) {
+  // webpackBootstrap
+  /******/ "use strict";
+  /******/ // The module cache
+  /******/ var installedModules = {};
+  /******/
+  /******/ // The require function
+  /******/ function __webpack_require__(moduleId) {
+    /******/
+    /******/ // Check if module is in cache
+    /******/ if (installedModules[moduleId]) {
+      /******/ return installedModules[moduleId].exports;
+      /******/
+    }
+    /******/ // Create a new module (and put it into the cache)
+    /******/ var module = (installedModules[moduleId] = {
+      /******/ i: moduleId,
+      /******/ l: false,
+      /******/ exports: {},
+      /******/
+    });
+    /******/
+    /******/ // Execute the module function
+    /******/ var threw = true;
+    /******/ try {
+      /******/ modules[moduleId].call(
+        module.exports,
+        module,
+        module.exports,
+        __webpack_require__
+      );
+      /******/ threw = false;
+      /******/
+    } finally {
+      /******/ if (threw) delete installedModules[moduleId];
+      /******/
+    }
+    /******/
+    /******/ // Flag the module as loaded
+    /******/ module.l = true;
+    /******/
+    /******/ // Return the exports of the module
+    /******/ return module.exports;
+    /******/
+  }
+  /******/
+  /******/
+  /******/ __webpack_require__.ab = __dirname + "/";
+  /******/
+  /******/ // the startup function
+  /******/ function startup() {
+    /******/ // Load entry module and return exports
+    /******/ return __webpack_require__(239);
+    /******/
+  }
+  /******/
+  /******/ // run startup
+  /******/ return startup();
+  /******/
+})(
+  /************************************************************************/
+  /******/ {
+    /***/ 239: /***/ function (
+      __unusedmodule,
+      __unusedexports,
+      __webpack_require__
+    ) {
+      //file that will run
+      const core = __webpack_require__(990);
+      const github = __webpack_require__(690);
 
-/***/ 239:
-/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
+      try {
+        // throw ( new Error ("some error msg"));
+        const name = core.getInput("who-to-greet");
+        console.log(`Hello ${name}`);
 
-//file that will run
-const core = __webpack_require__(990);
-const github = __webpack_require__(690);
+        const time = new Date();
+        core.setOutput("Time", time.toTimeString());
 
-try {
-   // throw ( new Error ("some error msg"));
-    const name = core.getInput('who-to-greet');
-    console.log(`Hello ${name}`);
-    
-    const time = new Date();
-    core.setOutput("Time", time.toTimeString());
-    
-    console.log(JSON.stringify(github, null, '\t'));
-    
-} catch (error) {
-    core.setFailed(error.message)
-}
+        console.log(JSON.stringify(github, null, "\t"));
+      } catch (error) {
+        core.setFailed(error.message);
+      }
 
+      /***/
+    },
 
+    /***/ 690: /***/ function (module) {
+      module.exports = eval("require")("@actions/github");
 
+      /***/
+    },
 
+    /***/ 990: /***/ function (module) {
+      module.exports = eval("require")("@actions/core");
 
+      /***/
+    },
 
-/***/ }),
-
-/***/ 690:
-/***/ (function(module) {
-
-module.exports = eval("require")("@actions/github");
-
-
-/***/ }),
-
-/***/ 990:
-/***/ (function(module) {
-
-module.exports = eval("require")("@actions/core");
-
-
-/***/ })
-
-/******/ });
+    /******/
+  }
+);
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,21 +1,16 @@
 //file that will run
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
-   // throw ( new Error ("some error msg"));
-    const name = core.getInput('who-to-greet');
-    console.log(`Hello ${name}`);
-    
-    const time = new Date();
-    core.setOutput("Time", time.toTimeString());
-    
-    console.log(JSON.stringify(github, null, '\t'));
-    
+  // throw ( new Error ("some error msg"));
+  const name = core.getInput("who-to-greet");
+  console.log(`Hello ${name}`);
+
+  const time = new Date();
+  core.setOutput("Time", time.toTimeString());
+
+  console.log(JSON.stringify(github, null, "\t"));
 } catch (error) {
-    core.setFailed(error.message)
+  core.setFailed(error.message);
 }
-
-
-
-
